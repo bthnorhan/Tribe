dd92b86f59f1c86f80ff63c30a76b15a61dc8b1e
Batuhan Orhan <bthnorhan00@gmail.com>
2023-04-26 22:54:30 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const App = (): JSX.Element => {
+	return <SafeAreaView></SafeAreaView>;
+};
+
+export default App;
